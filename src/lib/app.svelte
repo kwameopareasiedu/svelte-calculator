50df21c1feeabe4ea64e02dfcb85dee6212fc2fd
= <=>
2023-04-10 17:56:01 +0000

    Initial commit. Setup project. Configured Eslint, Prettier to work with Svelte. Installed TailwindCSS for styling

---
+++ src/lib/app.svelte
@@ -0,0 +1,16 @@
+<script lang="ts"></script>
+
+<main class="h-auto md:h-full md:flex">
+  <section
+    class="w-full grid place-items-center border-b-2 border-gray-100 md:border-e-2 md:max-w-screen-sm"
+  >
+    <div class="p-6 md:p-0">
+      <h3 class="font-bold text-4xl text-center">Svelte Calculator</h3>
+      <p class="text-center font-light">Kwame Opare Asiedu</p>
+    </div>
+  </section>
+
+  <section class="md:grow">
+    <p>Calculator goes here</p>
+  </section>
+</main>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import "./main.css";
+import App from "./lib/app.svelte";
+
+const app = new App({
+  target: document.getElementById("app"),
+});
+
+export default app;
