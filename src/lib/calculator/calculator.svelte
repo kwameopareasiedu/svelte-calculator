4b423053501e83fd8af583316fa303f73b52ae99
= <=>
2023-04-11 03:47:21 +0000

    Finished with calculator project

--- src/lib/calculator/calculator.svelte
+++ src/lib/calculator/calculator.svelte
@@ -4,8 +4,15 @@
 
   const opRegex = /[+|\-*\\/]/g;
   const operators = ["+", "-", "*", "/"];
+  const operatorFns = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+  };
 
   let operation = "";
+  let result = "";
   $: lastComponent = operation.endsWithAnyOf(operators)
     ? operation.slice(-1)[0]
     : operation.split(opRegex).slice(-1)[0];
@@ -22,12 +29,16 @@
 
     if (val === "Escape") {
       operation = "";
+      result = "";
     } else if (val === "Backspace") {
       operation = operation.slice(0, -1);
     } else if (val === "=") {
-      operation = compute().toString();
+      result = compute().toString();
+      // compute();
     } else if (operators.includes(val)) {
-      if (!lastComponentIsOperator) {
+      if (!operation) {
+        if (val === "-") operation += val;
+      } else if (!lastComponentIsOperator) {
         operation += val;
       }
     } else {
@@ -38,7 +49,7 @@
           operation += ".";
         }
       } else if (val === "%") {
-        if (!lastComponentIsPercentage) {
+        if (!lastComponentIsPercentage && operation) {
           operation += "%";
         }
       } else operation += val;
@@ -48,7 +59,96 @@
   function compute() {
     if (!operation) return 0;
 
+    const len = operation.length;
     const tokens = [];
+    let currentToken = "";
+    let currentTokenIdx = 0;
+    let startsWithNegative = false;
+    // const startsWithNegative = operation.startsWith("-");
+
+    while (currentTokenIdx < len) {
+      const char = operation[currentTokenIdx];
+
+      if (operators.includes(char)) {
+        if (currentTokenIdx === 0) {
+          startsWithNegative = true;
+        } else {
+          if (startsWithNegative) {
+            tokens.push("-" + currentToken);
+            startsWithNegative = false;
+          } else {
+            tokens.push(currentToken);
+          }
+
+          tokens.push(char);
+          currentToken = "";
+        }
+      } else currentToken += char;
+
+      currentTokenIdx += 1;
+    }
+
+    if (currentToken) {
+      tokens.push(currentToken);
+    }
+
+    while (tokens.length > 1) {
+      for (const operator of operators) {
+        const operatorFn = operatorFns[operator];
+        const operatorIndex = tokens.indexOf(operator);
+
+        if (operatorIndex > -1) {
+          const rawOp1 = tokens[operatorIndex - 1];
+          const rawOp2 = tokens[operatorIndex + 1];
+          const op1 = rawOp1.includes("%")
+            ? 0.01 * parseFloat(rawOp1)
+            : parseFloat(rawOp1);
+          const op2 = rawOp2.includes("%")
+            ? 0.01 * parseFloat(rawOp2)
+            : parseFloat(rawOp2);
+          tokens.splice(operatorIndex - 1, 3, operatorFn(op1, op2));
+          break;
+        }
+      }
+
+      /*const divisionIndex = tokens.indexOf("/");
+
+      if (divisionIndex > -1) {
+        const op1 = parseFloat(tokens[divisionIndex - 1]);
+        const op2 = parseFloat(tokens[divisionIndex + 1]);
+        tokens.splice(divisionIndex - 1, 3, op1 / op2);
+        continue;
+      }
+
+      const multiplicationIndex = tokens.indexOf("*");
+
+      if (multiplicationIndex > -1) {
+        const op1 = parseFloat(tokens[multiplicationIndex - 1]);
+        const op2 = parseFloat(tokens[multiplicationIndex + 1]);
+        tokens.splice(multiplicationIndex - 1, 3, op1 * op2);
+        continue;
+      }
+
+      const additionIndex = tokens.indexOf("+");
+
+      if (additionIndex > -1) {
+        const op1 = parseFloat(tokens[additionIndex - 1]);
+        const op2 = parseFloat(tokens[additionIndex + 1]);
+        tokens.splice(additionIndex - 1, 3, op1 + op2);
+        continue;
+      }
+
+      const subtractionIndex = tokens.indexOf("-");
+
+      if (subtractionIndex > -1) {
+        const op1 = parseFloat(tokens[subtractionIndex - 1]);
+        const op2 = parseFloat(tokens[subtractionIndex + 1]);
+        tokens.splice(subtractionIndex - 1, 3, op1 - op2);
+        continue;
+      }*/
+    }
+
+    return Number(tokens[0]);
   }
 </script>
 
@@ -56,7 +156,9 @@
   <!-- Screen -->
   <div class="w-full pt-24 md:pt-32 mb-4">
     <!-- Result -->
-    <h1 class="text-end text-7xl font-light font-mono mb-6 text-white">0</h1>
+    <h1 class="text-end text-7xl font-light font-mono mb-6 text-white">
+      {result || "0"}
+    </h1>
     <!-- Operation -->
     <p class="text-end text-gray-400">{operation || "0"}</p>
   </div>
